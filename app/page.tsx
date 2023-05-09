73115cc79a3ac691cec1964a3946ecc98606ef97
bbggkkk <bbggkkk@naver.com>
2023-05-09 18:50:41 +0900

    fix: fetch test

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
+"use client"
+
 import React from 'react';
 
 export default function Page() {
-    return <h1>Hello, Next.js!</h1>;
+    return (
+      <>
+        <h1>Hello, Next.js!</h1>
+        <button onClick={() => {
+          fetch('/api/test');
+        }}>hi</button>
+      </>
+    );
   }
