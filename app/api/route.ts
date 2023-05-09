1560a32b564d8665a09a66c62c56d5c03ac5c5d5
bbggkkk <bbggkkk@naver.com>
2023-05-09 20:06:41 +0900

    feat: node에서 파이썬 호출 테스트

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,6 +1,23 @@
 import { NextResponse } from 'next/server';
+import { spawn } from 'child_process';
+import { resolve } from 'path';
 
 export async function GET(request: Request) {
-    console.log(request);
-    return NextResponse.json({ data: 'good' });
+    const {stdout, ...python} = spawn('python', [resolve('src/test.py')]);
+    const data = await new Promise((res, rej) => {
+        let result = '';
+        stdout.on('data', (data) => {
+            result = data.toString().trim();
+        });
+        stdout.on('close', () => {
+            res(result);
+        });
+        stdout.on('error', (err) => {
+            rej(err);
+        });
+    }).catch((err) => {
+        console.error(err);
+        return false
+    }); 
+    return NextResponse.json({ data });
 }
