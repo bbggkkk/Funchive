43fa7a12a0fc8a2f2b36817a80deb55dfd668546
bbggkkk <bbggkkk@naver.com>
2023-05-09 20:18:47 +0900

    feat: 배치파일 가상환경 실행 명령어 수정

--- run.bat
+++ run.bat
@@ -1,8 +1,6 @@
 python -m venv .pyenv
-.pyenv\Scripts\activate
-pip install --upgrade pip
+.pyenv\Scripts\activate | pip install --upgrade pip
 pip install -U -r req.txt
 nodeenv .nodeenv
-.nodeenv\Scripts\activate
-npm install
+.nodeenv\Scripts\activate | npm install
 npm run dev
